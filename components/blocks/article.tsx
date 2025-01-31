2e24b78ef4e7b6c5ab3a648a72f66e53120b9cbf
namecoder1 <bartolomei.private@gmail.com>
2025-01-31 19:45:29 +0100

    fix article bug on different broswer

--- components/blocks/article.tsx
+++ components/blocks/article.tsx
@@ -16,10 +16,12 @@ const Article = ({ article } : any ) => {
 	 } = article
 	return (
 		<article className="fade-in-alternate">
-			<Link href={`/blog/${category.slug}`} className="relative w-full -z-0">
-				<Image priority src={image} alt={imageAlt} width={500} height={400} className='w-full shadow-md rounded-xl border border-gray-300' />			
-				<Pin className="absolute top-3 right-3" title={category.title}/>
-			</Link>
+			<div className="relative">
+					<Image priority src={image} alt={imageAlt} width={500} height={400} className='w-full shadow-md rounded-xl border border-gray-300' />			
+				<Link href={`/blog/${category.slug}`}>
+					<Pin className="absolute top-3 right-3" title={category.title}/>
+				</Link>
+			</div>
 			<h2 className="heading-sm mt-2">{title}</h2>
 			<p className="line-clamp-3 mt-2">{excerpt}</p>
 			<Button asChild className="w-full mt-3">
